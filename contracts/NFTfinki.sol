eba3467a410e4043c806f3188255c2f122e27753
IvanaShishoska <63503356+IvanaShishoska@users.noreply.github.com>
2022-07-12 14:08:42 +0200

    Add files via upload

---
+++ contracts/NFTfinki.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract FinkiNFT is ERC721Enumerable, Ownable {
+
+    string private _baseTokenURI;
+
+    constructor() ERC721("Finki NFT","FINKI") {
+        setBaseURI("https://gateway.pinata.cloud/ipfs/QmducRYQeWxmfuV9VfiVQ4uGTNz6g8eQb5P6oqshNEZdp5");
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseTokenURI, Strings.toString(_tokenId)));
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function mint() public {
+        uint256 totalSupply = totalSupply();
+        _safeMint(msg.sender, totalSupply + 1);
+    }
+
+}
